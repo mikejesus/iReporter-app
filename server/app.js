bf00ceb78d361205cf0174dabf16835608702d01
Michael Sunday <olawuni.michael@gmail.com>
2020-05-20 11:30:14 +0100

    chore: Set up the server in app.js

---
+++ server/app.js
@@ -0,0 +1,23 @@
+//import express
+import express from 'express';
+
+
+//Set up the express app
+const app = express();
+
+//Parse incoming request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Basic test route
+app.get('/', (req, res) => {
+    res.send("Hello there... I'm Michael");
+});
+
+//Set Port
+const PORT = 5000;
+
+//Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
